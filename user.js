77fe3a08ae1053b91617068596cd11da6b0210fb
Sagar-56 <sb0473564@gmail.com>
2022-11-24 11:02:58 +0530

    make it better

---
+++ user.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   userId: String,
+//   phone: Number,
+//   email: String,
+//   password: String,
+// });
+
+// module.exports = mongoose.model("users", userSchema);
