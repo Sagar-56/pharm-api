c8feb300f395c091afdec66e02e335a6b62b3bf5
Sagar-56 <sb0473564@gmail.com>
2022-12-04 09:20:58 +0530

    make it better

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("Let's Express")
 })
+const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true});
 
-MongoClient.connect(mongoUrl,async (err, client) => {
+client.connect((err) => {
     if (err) console.log(`Error While Connecting`);
-    db =await client.db('eduInternJan');
+    const db = client.db('eduInternJan');
     app.listen(port, () => {
         console.log(`server is running on port ${port}`)
     })
